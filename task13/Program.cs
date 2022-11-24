41ab151a75f890049c686890405e4e68f00300bc
tatangracheva <grachtany@yandex.ru>
2022-11-24 23:29:10 +0300

    Сделано задание найти третью цифру числа

---
+++ task13/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+Console.Clear();
+
+Console.Clear();
+Console.Write("Введите число a - ");
+
+string mas = Console.ReadLine();
+
+int n = mas.Length;
+int index = 0;
+
+if (n>2)
+    Console.WriteLine ($"Третья цифра заданного числа {mas[2]}");
+else 
+    Console.WriteLine ($"Третья цифра заданного числа отсутствует");
