bed5cb0b8b9b87c6374f7f422ebb4c32c2acb312
tatangracheva <grachtany@yandex.ru>
2022-11-24 22:27:29 +0300

    Вывод второй цифры трехзначного числа

---
+++ task1/Program.cs
@@ -0,0 +1,6 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+int numberA = new Random().Next (100, 1000)  ;
+Console.WriteLine($"Случайное число {numberA}");
+int result = (numberA/10)%10;
+Console.WriteLine ($"Вторая цифра числа {numberA} - {result}");
