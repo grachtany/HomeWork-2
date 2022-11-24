df04c728045b003d2d8a9c4cf4aff46cf0635360
tatangracheva <grachtany@yandex.ru>
2022-11-24 23:40:59 +0300

    определение выходного дня

---
+++ task15/Program.cs
@@ -0,0 +1,9 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+Console.Clear();
+Console.Write("Введите номер дня недели - ");
+int number1 = int.Parse(Console.ReadLine());
+if (1<=number1 && number1<=5 ) 
+    Console.WriteLine ($"День недели под номером {number1} - будний день");
+    else 
+        Console.WriteLine ($"День недели под номером {number1} - выходной день");
